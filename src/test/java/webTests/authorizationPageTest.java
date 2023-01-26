f4ffeea248715d7a50eaa51599367b5052bde2a6
SotQA <108469474+SotQA@users.noreply.github.com>
2023-01-26 10:46:13 +0100

    Update authorizationPageTest.java

--- src/test/java/webTests/authorizationPageTest.java
+++ src/test/java/webTests/authorizationPageTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class authorizationPageTest extends WebElements {
+public class authorizationPageTest {
     static WebDriver driver;
 
     @BeforeAll
